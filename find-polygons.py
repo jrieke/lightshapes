c37aee4e3f2b17908f8ad44e6a2c083409da9b47
Johannes Rieke <johannes.rieke@gmail.com>
2019-02-16 01:46:46 +0100

    Changed arrow key use

--- find-polygons.py
+++ find-polygons.py
@@ -6,12 +6,12 @@ import colors
 # --------- Settings --------------
 debug = True
 filename = 'polygons.json'
-#width, height = 640, 480
-width, height = 1920, 1080
+width, height = 640, 480
+#width, height = 1920, 1080
 # ---------------------------------
 
 
-print('To position the cursor: Click with the mouse or use arrow keys (shift+arrow keys for quick movement)')
+print('To position the cursor: Click on the screen or use shift+arrow keys for quick movement, use arrow keys for fine adjustment')
 
 x, y = 0, 0
 polygons = []
@@ -50,27 +50,37 @@ while 1:
                     print('Removed polygon with points:', polygons[-1])
                     del polygons[-1]
                     save_polygons()
+            # If arrow keys are pressed, move pointer slowly.
+            elif event.key == pygame.K_LEFT and x > 0:
+                x -= 1
+            elif event.key == pygame.K_RIGHT  and x < width:
+                x += 1
+            elif event.key == pygame.K_UP and y > 0:
+                y -= 1
+            elif event.key == pygame.K_DOWN and y < height:
+                y += 1
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            #print(event.pos)
-            #print('before', x, y)
             x, y = event.pos
-            #print('after', x, y)
 
-    # If arrow keys are pressed, move pointer.
+    # If alt and arrow keys are pressed, move pointer quickly.
     keys = pygame.key.get_pressed()
-    offset = 7 if pygame.key.get_mods() & pygame.KMOD_SHIFT else 1
-    if keys[pygame.K_LEFT] and x > 0:
-        x -= offset
-    if keys[pygame.K_RIGHT] and x < width:
-        x += offset
-    if keys[pygame.K_UP] and y > 0:
-        y -= offset
-    if keys[pygame.K_DOWN] and y < height:
-        y += offset
+    #offset = 7 if pygame.key.get_mods() & pygame.KMOD_SHIFT else 1
+    offset = 7
+    if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+        if keys[pygame.K_LEFT] and x > 0:
+            x -= offset
+        if keys[pygame.K_RIGHT] and x < width:
+            x += offset
+        if keys[pygame.K_UP] and y > 0:
+            y -= offset
+        if keys[pygame.K_DOWN] and y < height:
+            y += offset
 
     # Draw everything.
     screen.fill(colors.black)
-    pygame.draw.rect(screen, colors.red, (x, y, 3, 3))
+    pygame.draw.circle(screen, colors.red, (x, y), 2)
+    #pygame.draw.line(screen, colors.red, (x-50, y), (x+50, y))
+    #pygame.draw.line(screen, colors.red, (x, y-50), (x, y+50))
     pygame.draw.circle(screen, colors.red, (x, y), 50, 3)
     pygame.draw.circle(screen, colors.red, (x, y), 10, 2)
     for point in current_polygon:
